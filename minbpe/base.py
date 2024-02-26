1b7e6423358725920620eb87cf977efc6a322550
Ah-Joee <joseph3806@berkeley.edu>
2024-02-26 11:35:08 -0800

    helper functions

--- minbpe/base.py
+++ minbpe/base.py
@@ -39,3 +39,30 @@ def merge(ids, pair, idx):
             newids.append(ids[i])
             i += 1
     return newids
+
+
+def replace_control_characters(s: str) -> str:
+    """ 
+    Given a string, replace all the control characters into 
+    an escape
+    """
+    # we don't want to print control characters
+    # which distrot the output (e.g., \n or much worse)
+    chars = []
+    for ch in s:
+        # unicodedata.category(ch)[0] will return 'C' if it is a control character
+        if unicodedata.category(ch)[0] != 'C':
+            chars.append(ch) # this character is ok
+        else:
+            chars.append(f"\\u{ord(ch):04x}") # escape
+    return "".join(chars)
+
+
+def render_token(t: bytes) -> str:
+    """ 
+    Given a utf-8 encoding messge, return it's string format
+    """
+    # Pretty print a toke, escaping control characters
+    s = t.decode('utf-8', errors='replace')
+    s = replace_control_characters(s)
+    return s
